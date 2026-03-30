8a46d78fcf178ae86e4dc0aff19437e803437321
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2026-03-30 10:19:39 +0530

    Fix formatting and improve message clarity in song.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- SPOTIFY_MUSIC/plugins/tools/song.py
+++ SPOTIFY_MUSIC/plugins/tools/song.py
@@ -13,10 +13,7 @@ async def s(_,m:Message):
     if len(m.command)<2:return await m.reply("❌ Usage: /song song name")  
     r=(await VideosSearch(" ".join(m.command[1:]),limit=10).next())["result"]  
     b=[[InlineKeyboardButton(f"{i+1}. {x['title'][:40]}",callback_data=f"song_{x['id']}")] for i,x in enumerate(r)]  
-    txt=f"🎧 {m.from_user.mention}
-
-✨ Here is your song list
-Tap below to download & enjoy smooth offline vibes 💫"  
+    txt=f"🎧 {m.from_user.mention}\n\n✨ Here is your song list\nTap below to download & enjoy smooth offline vibes 💫"  
     await m.reply(txt,reply_markup=InlineKeyboardMarkup(b))  
   
 @app.on_callback_query(filters.regex("^song_"))  
@@ -50,9 +47,7 @@ async def f(m:Message,p:str,v:str):
         t=(await VideosSearch(v,limit=1).next())["result"][0]["title"]  
     except:  
         t=f"Song - {v}"  
-    caption=f"🎵 **{t[:90]}**
-
-💫 **BabiesIQ Music Bot**"  
+    caption=f"🎵 **{t[:90]}**\n\n💫 **BabiesIQ Music Bot**"  
     try:  
         with open(p,"rb") as audio_file:  
             msg = await m.reply_audio(  
